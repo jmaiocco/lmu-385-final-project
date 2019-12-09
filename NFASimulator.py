17eab61cba4437c67f0b4f383a3b1e0bb643ce7b
Joseph Maiocco <33736943+jmaiocco@users.noreply.github.com>
2019-12-09 13:24:11 -0800

    Update functional (but not clean) code

--- NFASimulator.py
+++ NFASimulator.py
@@ -21,7 +21,7 @@ class Nondeterministic_Finite_State_Machine:
                         symbol = split_line[1]
                         new_state = split_line[2]
                     else:     
-                        symbol = ""
+                        symbol = "_"
                         new_state = split_line[1]
                     if not (state in self.transitions.keys()):
                     	self.transitions[state] = { symbol: [new_state] }
@@ -33,22 +33,42 @@ class Nondeterministic_Finite_State_Machine:
         print("START STATE IS " + self.start_state)
         print("SET OF ACCEPT STATES IS " + str(self.accept_states))
         print("TRANSITIONS ARE " + str(self.transitions))
-        print("\nMACHINE CONSTRUCTED: TESTING INPUT STRING")
+        print("\nMACHINE CONSTRUCTED: TESTING INPUT STRING\n")
 
     def return_possible_transition(self, symbol, current_state):
-        return self.transitions[current_state][symbol]
+        try: 
+            possible_transitions = self.transitions[current_state][symbol]
+            return possible_transitions
+        except Exception as error:
+        	return []
 
     def accepts(self, input_string, state="q0"):
         if( len(input_string) == 0 and state in self.accept_states ):
         	return True
+        possible_moves = {}
+        #print("Current state: " + state)
+        #print("Checking string: " + input_string)
         symbol = input_string[:1]
         next_possible_states = self.return_possible_transition(symbol, state)
+        #print(next_possible_states)
+        if not next_possible_states:
+            return False
         for new_state in next_possible_states:
-        	if(c.accepts(input_string[1:], new_state)):
-        		return True
+            possible_moves[new_state] = self.accepts(input_string[1:], new_state)
+       	#print(possible_moves)
+        if True in possible_moves.values():
+            correct_paths = [state for state,success in possible_moves.items() if success == True]
+            for next_state in correct_paths:
+                return self.accepts(input_string[1:], next_state)
         return False
 
 
+
 c = Nondeterministic_Finite_State_Machine()
-print(c.accepts("a", c.start_state))
-print(c.accepts("", c.start_state))
+print(c.accepts(sys.argv[2]))
+#print(c.accepts("a", c.start_state))		#TRUE
+#print(c.accepts("aaaaa", c.start_state))	#TRUE
+#print(c.accepts("b", c.start_state))		#FALSE
+#print(c.accepts("_", c.start_state))		#TRUE
+#print(c.accepts("__", c.start_state)) 		#FALSE
+#print(c.accepts("aaaaaaaaaaaaaaa", c.start_state))	#TRUE
