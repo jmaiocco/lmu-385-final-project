fc0ef2b7994d44b2d14cd9ff616775fae37d78c7
Joseph Maiocco <33736943+jmaiocco@users.noreply.github.com>
2019-12-09 19:49:43 -0800

    Update working code prior to optimization

--- nfa_command_line.py
+++ nfa_command_line.py
@@ -18,23 +18,21 @@ def take_input_or_test_machine(command_line_arg_count):
         nfa = NondeterministicFiniteStateMachine()
         check_if_in_language(nfa)
     else:
-        print("Please make sure you are using the correct number of command line arguments.")
+        print("\nPlease make sure you are using the correct number of command line arguments.")
 
 def check_if_in_language(nfa):
     if nfa.accepts(sys.argv[1]):
-        print("TRUE: That string is accepted by the given NFA.")
+        print("\nTRUE: That string is accepted by the given NFA.")
     else:
-        print("FALSE: That string is NOT accepted by the given NFA.")
+        print("\nFALSE: That string is NOT accepted by the given NFA.")
 
 class NondeterministicFiniteStateMachine:
     def __init__(self):
         self.transitions = {}
         file = sys.stdin
-        i = 0
         for line in file:
             split_line = re.split(r'[=:;\->\n]+', line)
             split_line = list(filter(None, split_line))
-            print(split_line)
             if split_line[0].startswith('START'):
                 self.start_state = split_line[1]
                 if "," in split_line[3]:
@@ -55,11 +53,7 @@ class NondeterministicFiniteStateMachine:
                     self.transitions[state][symbol] = [new_state]
                 else:
                     self.transitions[state][symbol].append(new_state)
-            i += 1
-        print("START STATE IS " + self.start_state)
-        print("SET OF ACCEPT STATES IS " + str(self.accept_states))
-        print("TRANSITIONS ARE " + str(self.transitions))
-        print("\nMACHINE CONSTRUCTED: TESTING INPUT STRING\n")
+
 
     def return_possible_transition(self, symbol, current_state):
         try:
@@ -72,16 +66,17 @@ class NondeterministicFiniteStateMachine:
         if len(input_string) == 0 and state in self.accept_states:
             return True
         possible_moves = {}
-        symbol = input_string[:1]
-        next_possible_states = self.return_possible_transition(symbol, state)
+        consumed_symbol = input_string[:1]
+        next_possible_states = self.return_possible_transition(consumed_symbol, state)
         if not next_possible_states:
             return False
+        remaining_string = input_string[1:]
         for new_state in next_possible_states:
-            possible_moves[new_state] = self.accepts(input_string[1:], new_state)
+            possible_moves[new_state] = self.accepts(remaining_string, new_state)
         if True in possible_moves.values():
             correct_paths = [state for state, success in possible_moves.items() if success is True]
             for next_state in correct_paths:
-                return self.accepts(input_string[1:], next_state)
+                return self.accepts(remaining_string, next_state)
         return False
 
 
